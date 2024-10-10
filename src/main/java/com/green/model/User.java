4baa02516e8f3db7524ad4e5bbb316c8db788290
Dhanashri Subhash Nimbalakar <dhanashri.10820652@ltimindtree.com>
2024-10-10 23:07:14 +0530

    first push

---
+++ src/main/java/com/green/model/User.java
@@ -0,0 +1,13 @@
+package com.green.model;
+
+
+public class User {
+	
+	private int id;
+	private String name;
+	private String pan_no;
+	private String email;
+	private int pin_no;
+	private long acc_no;
+
+}
