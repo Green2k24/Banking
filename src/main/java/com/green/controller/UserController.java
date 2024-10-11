cfefc9e6df0bcf576ad1282402b47e8f2506d3c2
10820652 <10820652@5CD0485MFJ>
2024-10-11 15:09:55 +0530

    Created structure and added endpoints

--- src/main/java/com/green/controller/UserController.java
+++ src/main/java/com/green/controller/UserController.java
@@ -1,14 +1,43 @@
 package com.green.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.green.model.User;
 
 @RestController
 public class UserController {
 	
-	@GetMapping("/test")
-	public String test() {
+	
+	@PostMapping("/registerUser")
+	public String registerUser(@RequestBody User user) {	
+		return "register successfully";
+	}
+	
+	@PostMapping("/login")
+	public String login() {
 		return "success";
 	}
-
+	
+	@PostMapping("/withdraw")
+	public String withdraw(){
+		return "withdraw";
+	}
+	
+	@PostMapping("/deposit")
+	public String deposit(){
+		return "deposit";
+	}
+	
+	@GetMapping("/history")
+	public String history(){
+		return "withdraw";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(){
+		return "logout";
+	}
 }
--- src/main/java/com/green/model/User.java
+++ src/main/java/com/green/model/User.java
@@ -5,9 +5,7 @@ public class User {
 	
 	private int id;
 	private String name;
-	private String pan_no;
 	private String email;
-	private int pin_no;
-	private long acc_no;
+	private String password;
 
 }
